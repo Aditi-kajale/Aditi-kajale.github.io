f78661dec60ff9b5a93427b29519c12c5ec98d0e
Aditi Kajale <kajaleaditi01@gmail.com>
2025-10-19 13:02:51 -0400

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,8 +8,9 @@ export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {}
+  
+          <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+          {emoji("Thank you :)")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
